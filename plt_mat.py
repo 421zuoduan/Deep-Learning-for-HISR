53ab9e860358d137ec9833e28813baca617806bd
421zuoduan <2826314784@qq.com>
2023-10-08 20:05:04 +0800

    update

--- plt_mat.py
+++ plt_mat.py
@@ -1,25 +1,31 @@
 import scipy.io
 import matplotlib.pyplot as plt
+import numpy as np
 
 # 读取MAT文件
-mat_data = scipy.io.loadmat('my_model_results\Bidi_merge1_xca_group1_light_48\AdaTrans\cave_x4\Bidi_merge1_xca_group1_light_48_cave2000.mat')
-# mat_data = scipy.io.loadmat('my_model_results\Bidi_kernelattentionv2\AdaTrans\cave_x4\Bidi_kernelattentionv2_cave2000.mat')
+mat_data = scipy.io.loadmat('my_model_results/Bidi_kernelattentionv2/AdaTrans/cave_x4/Bidi_kernelattentionv2_cave2000.mat')
 
 # 假设MAT文件中的图像存储在变量 'images' 中
 images = mat_data['output']
 
-# 显示图像
+# 取出要作为RGB通道的通道索引（第31/20/10个通道）
+channel_indices = [30, 19, 9]
+# channel_indices = [1, 12, 22]
+
+# 创建一个2x6的子图布局，以容纳所有图片
 num_images = images.shape[0]
-
-# 创建一个2x6的子图布局，可以根据需要调整行数和列数
 rows, cols = 2, 6
-plt.figure(figsize=(12, 6))
+fig, axs = plt.subplots(rows, cols, figsize=(12, 6))
 
+# 遍历每张图片并将其显示在子图中
 for i in range(num_images):
-    plt.subplot(rows, cols, i + 1)
-    plt.imshow(images[i, :, :, 0], cmap='R-1')  # 显示第一个通道的图像
-    plt.title(f'Image {i + 1}')
-    plt.axis('off')
+    row, col = divmod(i, cols)  # 计算子图的行和列
+    rgb_channels = [images[i, :, :, index] for index in channel_indices]
+    rgb_image = np.stack(rgb_channels, axis=-1)
+    
+    axs[row, col].imshow(rgb_image)
+    axs[row, col].set_title(f'Image {i + 1}')
+    axs[row, col].axis('off')
 
 plt.tight_layout()
 plt.show()
